b182f5ee3064b90278affb460d902079c7b489b2
Adam Patterson <adam.patterson0012@gmail.com>
2014-11-04 21:37:45 -0500

    refactors tests to use helper methods

---
+++ spec/features/user_creates_todo_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+feature "User creates todo" do
+  scenario "successfully" do
+    sign_in
+
+    create_todo "Buy milk"
+
+    expect(page).to display_todo "Buy milk"
+  end
+end
